39bb6d53ac6328e28507d90631f771aa87434d66
fernandofreitas03 <fernandofreitas130gmail.com>
2022-07-27 18:17:58 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import People from "./assets/avatar1.svg";
+import Arrow from "./assets/arrow.svg";
+import Trash from "./assets/trash.svg";
+
+import {
+  H1,
+  Container,
+  InputLabel,
+  Input,
+  Div,
+  Button,
+  Image,
+  Users,
+} from "./styles";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const inputName = useRef();
+  const inputAge = useRef();
+
+  function addNewUser() {
+    setUsers([
+      ...users,
+      {
+        id: Math.random(),
+        name: inputName.current.value ,
+        age: inputAge.current.value ,
+      },
+    ]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Image alt="avatar" src={People} />
+      <Div>
+        <H1>Olá</H1>
+
+        <InputLabel>Nome</InputLabel>
+        <Input ref={inputName} placeholder="Nome" type="text" />
+
+        <InputLabel>Idade</InputLabel>
+        <Input ref={inputAge} placeholder="Idade" type="number" />
+
+        <Button onClick={addNewUser}>
+          Cadastrar
+          <img alt="seta" src={Arrow} />
+        </Button>
+
+        <ul>
+          {users.map((user) => (
+            <Users key={user.id}>
+              <p>{user.name}</p> <p>{user.age}</p>
+              <button>
+                <img src={Trash} />
+              </button>
+            </Users>
+          ))}
+        </ul>
+      </Div>
+    </Container>
   );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,144 @@
+import styled from "styled-components";
+import Background2 from "./assets/background2.svg";
+
+export const Container = styled.div`
+  background: url("${Background2}");
+  background-size: cover;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+`;
+export const Image = styled.img`
+  margin-top: 30px;
+`;
+
+export const Div = styled.div`
+  width: 414px;
+  height: 845px;
+
+  background: linear-gradient(
+    157.44deg,
+    rgba(255, 255, 255, 0.6) 0.84%,
+    rgba(255, 255, 255, 0.6) 0.85%,
+    rgba(255, 255, 255, 0.15) 100%
+  );
+  border-radius: 61px 61px 0px 0px;
+
+  padding: 50px 36px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const H1 = styled.h1`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 34px;
+  line-height: 40px;
+
+  text-align: center;
+  margin-bottom: 80px;
+
+  color: #ffffff;
+`;
+
+export const InputLabel = styled.p`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 22px;
+  letter-spacing: -0.408px;
+
+  padding-left: 25px;
+  color: #eeeeee;
+`;
+
+export const Input = styled.input`
+  width: 342px;
+  height: 58px;
+
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 14px;
+  border: none;
+
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 28px;
+
+  color: #ffffff;
+  outline: none;
+  margin-bottom: 34px;
+  padding-left: 25px;
+`;
+
+export const Button = styled.button`
+  width: 342px;
+  height: 74px;
+
+  margin-top: 132px;
+
+  background: rgba(0, 0, 0, 0.8);
+  border-radius: 14px;
+  border: none;
+  outline: none;
+
+  font-style: normal;
+  font-weight: 700;
+  font-size: 17px;
+
+  line-height: 28px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 14px;
+
+  color: #eeeeee;
+
+  &:hover {
+    opacity: 0.8;
+  }
+  &:active {
+    opacity: 0.6;
+  }
+`;
+
+export const Users = styled.li`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-top: 20px;
+
+  width: 342px;
+  height: 58px;
+
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 14px;
+
+  border: none;
+  outline: none;
+
+  color: #ffffff;
+
+  p {
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 28px;
+  }
+
+button{
+   width: 24px;
+   height: 28px;
+   
+   background: none;
+   border: none;
+   outline: none;
+
+   cursor: pointer;
+}
+
+`;
